9bcc23542d624eb3e0dd4417016f84458abb8424
Vyas Bhagwat <bhagwatvyas@users.noreply.github.com>
2018-05-07 20:24:35 -0700

    Adding Optimized Solution

--- Reverse Vowels.py
+++ Reverse Vowels.py
@@ -38,5 +38,25 @@ class Solution:
         return ''.join(x for x in s_list)
 
 
+    def reverseVowels_optimized(self,s):
+        # Since strings are immutable in Python, convert string to list
+        s_list = list(s)
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+        low = 0
+        high = len(s_list) - 1
+
+        while low < high:
+            while s_list[low] not in vowels and low < high:
+                low += 1
+            while s_list[high] not in vowels and low < high:
+                high -= 1
+            s_list[low], s_list[high] = s_list[high], s_list[low]
+            low += 1
+            high -= 1
+
+        return ''.join(x for x in s_list)
+
+
 sol = Solution()
 print(sol.reverseVowels("leetcode"))
